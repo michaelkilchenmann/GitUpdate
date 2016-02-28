a164a7078d823180385e112f2d7814c0a26da391
Michael Kilchenmann <michikilchenmann@gmail.com>
2016-02-29 00:16:27 +0100

    Init

---
+++ GitUpdate.sh
@@ -0,0 +1,6 @@
+
+
+cd "C:\Users\Michael Kilchenmann\Dropbox\R\AssetPriceForecasting"
+git add .
+git commit -m "comment"
+git push origin master
---
+++ git_bs.bat
@@ -0,0 +1,2 @@
+C:\Windows\system32\cmd.exe /c ""C:\Users\Michael Kilchenmann\AppData\Local\GitHub\PortableGit_c7e0cbde92ba565cb218a521411d0e854079a28c\git-bash.exe" -i -- C:\Scripts\GitUpdate\GitUpdate.sh"
+
